2c4c64a786e1a4807dabab77371cb30fcf688f02
Robert Norris <rob@eatenbyagrue.org>
2013-11-07 12:08:08 +1100

    add trailer video to the slider

--- lib/rocket/plugin/slideshow.pm
+++ lib/rocket/plugin/slideshow.pm
@@ -4,13 +4,33 @@
 #
 package rocket::plugin::slideshow;
 
+use 5.010;
 use warnings;
 use strict;
 
 use base qw(rocket::plugin);
 
+sub format_video {
+    my ($v) = @_;
+    my ($type, $id) = split ':', $v;
+    given ($type) {
+        when ("vimeo") {
+            return qq{
+                <iframe src="http://player.vimeo.com/video/$id?title=0&byline=0&portrait=0&badge=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            };
+        }
+        default {
+            die "unknown video type for '$v'";
+        }
+    }
+}
+
 sub content {
     my ($self, $stash, $postdata) = @_;
+
+    my @videos = qw(
+        vimeo:78452479
+    );
 
     opendir my $dir, "$rocket::SOURCE_DIR/assets";
     my @screenshots = sort grep { m/^ss\d+\.png$/ } readdir $dir;
@@ -19,6 +39,8 @@ sub content {
     my $html = join '', (
         '<ul id="slider">'
     );
+
+    do { $html .= q{<li>}.format_video($_).q{</li>} } for @videos;
 
     $html .= qq{<li><img src="$_"></li>} for map { "assets/$_" } @screenshots;
 
